231fbf0d4add1c66aae1db65ce0f7ba6cd1694cd
Asim Mehmood <asimm241@gmail.com>
2017-10-25 23:49:11 +0500

    main activity design grid layout and button added

--- app/src/main/java/com/example/asimm/wallet/SplashActivity.java
+++ app/src/main/java/com/example/asimm/wallet/SplashActivity.java
@@ -1,9 +1,17 @@
 package com.example.asimm.wallet;
 
 
+import android.os.Bundle;
+
 /**
  * Created by asimm on 10/9/2017.
  */
 
 public class SplashActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
 }
